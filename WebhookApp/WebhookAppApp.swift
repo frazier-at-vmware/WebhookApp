0f4c7de5ec61c12548f8daa5f1109ee24a253828
Frazier Smith <frazier@vfrazier.net>
2024-01-31 14:48:58 -0500

    Initial Commit

---
+++ WebhookApp/WebhookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebhookAppApp.swift
+//  WebhookApp
+//
+//  Created by Frazier Smith on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WebhookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
